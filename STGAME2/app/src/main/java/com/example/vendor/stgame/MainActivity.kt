768a45404b0fb6b9987962d4bd6de2a832e135cf
laiyu <383209920@qq.com>
2019-12-11 18:26:30 +0800

    增加沉浸式的title

--- STGAME2/app/src/main/java/com/example/vendor/stgame/MainActivity.kt
+++ STGAME2/app/src/main/java/com/example/vendor/stgame/MainActivity.kt
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.support.v7.app.AlertDialog
-import android.view.LayoutInflater
 import android.app.TimePickerDialog
 import android.app.Activity
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.support.v4.view.ViewCompat
 import android.text.TextUtils
 import android.util.Log
+import android.view.*
 import android.widget.*
 import java.util.*
 import java.util.concurrent.Executors
@@ -29,6 +32,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            setStatusBarColor(resources.getColor(R.color.greenEyeColor))
+        }
         initData()
         mData.sortWith(BossAdapter.mCompare)
         showLog(GsonUtils.toJson(mData))
@@ -36,6 +42,26 @@ class MainActivity : AppCompatActivity() {
         lv_boss_list.adapter = adapter
         bindEvent()
         OpenTask()
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private fun setStatusBarColor(statusColor: Int) {
+        val window = window
+        //取消状态栏透明
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        //添加Flag把状态栏设为可绘制模式
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        //设置状态栏颜色
+        window.statusBarColor = statusColor
+        //设置系统状态栏处于可见状态
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        //让view不根据系统窗口来调整自己的布局
+        val mContentView = window.findViewById<View>(Window.ID_ANDROID_CONTENT) as ViewGroup
+        val mChildView = mContentView.getChildAt(0)
+        if (mChildView != null) {
+            ViewCompat.setFitsSystemWindows(mChildView, false)
+            ViewCompat.requestApplyInsets(mChildView)
+        }
     }
 
     private fun OpenTask() {
